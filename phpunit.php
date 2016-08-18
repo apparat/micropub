d7224480e8c24f4676181e7d8dcaf6e0b21bad4f
Joschi Kuphal <joschi@tollwerk.de>
2016-08-18 22:03:43 +0200

    Experiments with PHPUnit & built-in webserver

--- phpunit.php
+++ phpunit.php
@@ -33,6 +33,21 @@
  *  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ***********************************************************************************/
 
+define('IS_WINDOWS', stripos(php_uname('s'), 'win') > -1);
+
+// Command that starts the built-in web server
+$command = sprintf('php -S %s:%d -t %s', WEB_SERVER_HOST, WEB_SERVER_PORT, WEB_SERVER_DOCROOT);
+$process = proc_open($command, [['pipe', 'r']], $pipes);
+$pstatus = proc_get_status($process);
+$pid = $pstatus['pid'];
+
+echo sprintf('%s - Web server started on %s:%d with PID %d', date('r'), WEB_SERVER_HOST, WEB_SERVER_PORT, $pid).PHP_EOL;
+
+register_shutdown_function(function () use ($pid) {
+    echo sprintf('%s - Killing process with ID %d', date('r'), $pid).PHP_EOL;
+    IS_WINDOWS ? exec("taskkill /F /T /PID $pid") : exec("kill -9 $pid");
+});
+
 error_reporting(E_ALL);
 $autoloader = __DIR__.'/vendor/autoload.php';
 if (!file_exists($autoloader)) {
